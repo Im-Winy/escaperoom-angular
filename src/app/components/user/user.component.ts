3d7ab1e7d297e5c5f538d22f259b30fa8d004362
WilliamNyacka <williamnyacka14@gmail.com>
2025-04-04 22:01:27 +0200

    new

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,26 +1,78 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { User } from '../../models/user/user.model';
-import { UserService } from '../../services/user/user.service';
-import { NotificationType } from '../../enum/notification-type.enum';
-import { HttpErrorResponse } from '@angular/common/http';
-import { NotificationService } from '../../services/notification/notification.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../models/user/user.model';
 
 @Component({
   selector: 'app-user',
-  imports: [FormsModule, CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor(private userService: UserService, private notificationService: NotificationService) { }
+  user!: User;
+  formUpdate!: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(private authenticationService: AuthenticationService, private fb: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
 
+    this.formUpdate = this.fb.group({
+      prenom: ['', Validators.required],
+      nom: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+    this.getUser();
   }
 
+  getUser(): void {
+    this.user = this.authenticationService.getUserFromLocalCache();
+    if (this.user) {
+      this.patchForm();
+    }
+  }
+
+  patchForm(): void {
+    this.formUpdate.patchValue({
+      prenom: this.user.prenom,
+      nom: this.user.nom,
+      username: this.user.username,
+      email: this.user.email
+    });
+  }
+
+  updateUser() {
+    if (this.formUpdate.invalid || !this.user.idUser) {
+      console.warn("Formulaire invalide ou utilisateur non sélectionné.");
+      return;
+    }
+    const formData = new FormData();
+    formData.append('prenom', this.formUpdate.value.prenom);
+    formData.append('nom', this.formUpdate.value.nom);
+    formData.append('username', this.formUpdate.value.username);
+    formData.append('email', this.formUpdate.value.email);
+
+    this.userService.updateUser(formData, this.user.idUser).subscribe({
+      next: (updatedUser) => {
+        this.user = updatedUser;
+        this.successMessage = 'Utilisateur mis à jour avec succès.';
+        this.errorMessage = '';
+        this.formUpdate.reset();
+        this.patchForm();
+      },
+      error: (error) => {
+        this.errorMessage = 'Erreur lors de la mise à jour de l\'utilisateur.';
+        this.successMessage = '';
+        console.error(error);
+      }
+    });
+  }
 }
