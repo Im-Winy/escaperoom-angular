65c15d099cfda59c76ff8d26beff669eeb36ad6c
WilliamNyacka <williamnyacka14@gmail.com>
2025-06-06 16:12:27 +0200

    new

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -20,8 +20,8 @@ export class AuthInterceptor implements HttpInterceptor {
     // Récupération du token JWT (par exemple depuis le localStorage via le service d'authentification)
     const token = this.authService.getToken();
 
-    // Vérifie si l'URL correspond à une route d'authentification (login), pour laquelle on ne doit pas ajouter de token
-    if (req.url.includes('/login') || req.url.includes('/authenticate')) {
+    // Vérifie si l'URL correspond à une route d'authentification (login/register), pour laquelle on ne doit pas ajouter de token
+    if (req.url.includes('/login') || req.url.includes('/register')) {
       // Si la requête est pour le login, on la laisse passer sans la modifier
       return next.handle(req);
     }
